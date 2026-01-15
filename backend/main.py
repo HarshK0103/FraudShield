c2bd2e785dcbacb9d20c1b0a17a2a4b4216ed61e
Harsh Karekar <hkarekar0103@gmail.com>
2026-01-16 01:25:23 +0530

    FraudShield files

--- backend/main.py
+++ backend/main.py
@@ -27,6 +27,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8080",
         "http://127.0.0.1:8080",
+        "https://fraud-shield-vert.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
